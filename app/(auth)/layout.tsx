faa3ffec04a0e33f020f14e7bd0bc07767b6150c
Koushik2208 <gorrekoushik@gmail.com>
2025-04-18 12:18:43 +0530

    added layout for auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+        return (
+        <div className="flex h-screen w-screen items-center justify-center">
+            {children}
+        </div>
+    );
+};
+
+export default AuthLayout;
+
